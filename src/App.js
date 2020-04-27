11ddde5ddc273bc0d9a2756271233bcf10d41d03
LeandroMDS <leandro.mds.21@gmail.com>
2020-04-27 20:35:55 -0300

    Challenge fixed

--- src/App.js
+++ src/App.js
@@ -4,40 +4,40 @@ import api from './services/api'
 import "./styles.css";
 
 function App() {
-  const [projects, setProjects] = useState([])
+  const [repository, setRepository] = useState([])
 
   useEffect(() => {
-   api.get('projects').then(response => {
-     setProjects(response.data)
+   api.get('repository').then(response => {
+     setRepository(response.data)
    })
   }, []);
 
   async function handleAddRepository() {
-    const response = await api.post('projects', {
+    const response = await api.post('repository', {
       title: `Novo Projeto create in ${Date.now()}`,
       owner: "Leandro M Silva"
     })
 
     const newProject = response.data
-    setProjects([...projects, newProject])
+    setRepository([...repository, newProject])
   }
 
   async function handleRemoveRepository(id) {
-    const response = await api.delete(`projects/${id}`)
+    const response = await api.delete(`repository/${id}`)
 
     if (!response.status === '204') { return }
 
-    const projectsCopy = projects
-    const projectDelIndex = projectsCopy.findIndex(project => project.id === id)
-    projectsCopy.splice(projectDelIndex, 1)
+    const repositoryCopy = repository
+    const projectDelIndex = repositoryCopy.findIndex(project => project.id === id)
+    repositoryCopy.splice(projectDelIndex, 1)
 
-    setProjects([...projectsCopy])
+    setRepository([...repositoryCopy])
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        { projects.map(project => (
+        { repository.map(project => (
           <li key={project.id}>
             {project.title}
 
