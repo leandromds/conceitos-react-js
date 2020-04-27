9243da7373a285c00defb87912a914430ebe1d7c
LeandroMDS <leandro.mds.21@gmail.com>
2020-04-27 10:14:14 -0300

    ajust test file App.test.js endpoint to projects instead repositories and  challange finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+   api.get('projects').then(response => {
+     setProjects(response.data)
+   })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('projects', {
+      title: `Novo Projeto create in ${Date.now()}`,
+      owner: "Leandro M Silva"
+    })
+
+    const newProject = response.data
+    setProjects([...projects, newProject])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`projects/${id}`)
+
+    if (!response.status === '204') { return }
+
+    const projectsCopy = projects
+    const projectDelIndex = projectsCopy.findIndex(project => project.id === id)
+    projectsCopy.splice(projectDelIndex, 1)
+
+    setProjects([...projectsCopy])
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        { projects.map(project => (
+          <li key={project.id}>
+            {project.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+            <button onClick={() => handleRemoveRepository(project.id)}>
+              Remover
           </button>
-        </li>
+          </li>
+        )) }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
